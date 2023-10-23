3880c9a4a5eeec4cea0aa67caca97fb3c08b5b40
anti1hero1 <nikitadanikita@gmail.com>
2023-10-23 13:24:37 +0300

    final

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import './Footer.css'
+import { Link, useLocation } from 'react-router-dom'
+
+
+export default function Footer() {
+  const { pathname } = useLocation()
+  return (
+    <footer className={`footer page__footer ${pathname === '/saved-movies' && 'page__footer_type_saved-movies'}`}>
+      <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__container">
+        <p className="footer__subtitle">© 2023</p>
+        <nav className="footer__nav">
+          <Link to={'https://practicum.yandex.ru/'} target='_blank' className="footer__link">Яндекс.Практикум</Link>
+          <Link to={'https://github.com/anti1hero1'} target='_blank' className="footer__link">Github</Link>
+        </nav>
+      </div>
+    </footer>
+  )
+}
